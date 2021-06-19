9f94b651228e8450058e9002e15493f7c05d7178
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2021-06-19 23:44:48 +0900

    파이썬 deploy 내용 추가

--- back_python_deploy1/2_python_anywhere1/flask_app.py
+++ back_python_deploy1/2_python_anywhere1/flask_app.py
@@ -2,8 +2,11 @@
 
 # pip install flask -> python -> import flask
 from flask import Flask, render_template
+import pymysql # pip install pymysql
 
 app = Flask(__name__)
+
+app.secret_key = b'zix7211'
 
 # # 기존 A5M2를 이용한 로컬환경용 DB
 # # conn = pymysql.connect(
@@ -14,7 +17,7 @@ app = Flask(__name__)
 # #     charset='utf8'
 # # )
 
-# # pythonanywhere 디플로이환경용 DB
+# # ★pythonanywhere 디플로이환경용 DB
 # conn = pymysql.connect(
 #     user='ydhvocal',
 #     passwd='zix7211!',
@@ -23,7 +26,9 @@ app = Flask(__name__)
 #     charset='utf8'
 # )
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
