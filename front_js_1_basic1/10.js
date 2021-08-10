7dfbf68369f3322b9f9e9474255f9f600d8b6704
azumaapp <ydhvocal@gmail.com>
2021-08-10 18:58:38 +0900

    10.js 클래스 추가

---
+++ front_js_1_basic1/10.js
@@ -0,0 +1,9 @@
+// calculator 객체(=클래스)는 plus에서 a + b를 반환한다. minus에서 a - b를 반환한다.
+const calculator = {
+    plus: function(a, b) {
+        return a + b
+    },
+    minus: function(a, b) {
+        return a - b
+    }
+}
