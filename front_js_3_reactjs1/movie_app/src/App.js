1ac25b9c1aca4ff115f9108b4cd1b299a3a9c6a5
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2021-10-24 00:49:47 +0900

    reactjs props

--- front_js_3_reactjs1/movie_app/src/App.js
+++ front_js_3_reactjs1/movie_app/src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Potato from "./Potato";
 
+function Food(props) {
+  // function Food(props.fav) = function Food({ fav }) {
+  console.log("props:", props)
+  console.log("props.fav:", props.fav)
+  return <h1>Potato is food!</h1>
+}
+
+function FavFood({ fav }) {
+  console.log("{ fav }:", { fav })
+  return <h1>My favorite food is {fav}</h1>
+}
+
 function App() {
   return (
     <div>
       Hello React!
       <Potato />
+      {/* ↓ fav등은 다 props(properties)이다. */}
+      <Food fav="kimchi" something={true} papapapa={["hello", 1, 2, 3, 4, true]} />
+      <FavFood fav="ramen" />
+      <FavFood fav="pizza" />
+      <FavFood fav="kimchi" />
     </div>
   );
 }
