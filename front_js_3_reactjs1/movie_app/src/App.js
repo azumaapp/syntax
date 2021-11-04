9b55dd5e255a3b75bc08d950f68ca10c8c7cb997
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2021-11-04 23:13:16 +0900

    reactjs State에 대해 알아야할 모든 것

--- front_js_3_reactjs1/movie_app/src/App.js
+++ front_js_3_reactjs1/movie_app/src/App.js
@@ -7,11 +7,17 @@ class App extends React.Component {
   state = {
     count: 0
   }
+  // currnet : 외부의 영향을 받지 않고 currentState로부터 데이터를 세팅할 수 있는 방식이다.
   add = () => {
     console.log("add")
+    // this.setState({count: this.state.count + 1}) // 기존의 좋지 않은 방식
+    this.setState(current => ({count: current.count + 1}))
+    console.log(this.state.count)
   }
   minus = () => {
     console.log("minus")
+    // this.setState({count: this.state.count - 1}) // 기존의 좋지 않은 방식
+    this.setState(current => ({count: current.count - 1}))
   }
   // 렌더를 통해서 데이터를 표시
   render(){
