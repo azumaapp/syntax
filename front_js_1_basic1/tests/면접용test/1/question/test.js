170cdf88afe7b04f6ca967edd2540a0d46cae614
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2021-11-14 17:05:40 +0900

    테스트 js

---
+++ front_js_1_basic1/tests/면접용test/1/question/test.js
@@ -0,0 +1,16 @@
+/**
+ * @param numbers Array<number> 配列
+ * @param target number 目標の数字
+ */
+ function sumExists(numbers, target) {
+}
+
+
+// 整数の配列が与えられます。配列から2つの異なる要素の和を取り、指定された数を作れるかどうかを教えてください。
+// 例：
+// [1, 2, 3], 3 => True (1 + 2 = 3)
+// [1, 1, 1], 2 => True (1 + 1 = 2)
+// [1, 1, 1], 3 => False
+// [1], 2 => False
+// [1, 1], 2 => True
+// [1, 3], 2 => False
