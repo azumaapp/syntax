58786a6a83b234857835fa0d9ae46a02183889b2
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2021-11-22 16:51:42 +0900

    reactJS: fetching movie JSON from api

--- front_js_3_reactjs1/movie_app/src/App.js
+++ front_js_3_reactjs1/movie_app/src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React from "react"
+import axios from "axios"
 
 // 앱
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
+  }
+  // axios : fetch 위에 있는 작은 layer, 설치 후 사용가능. npm i axios 후 사용.
+  // jsonview : 크롬 확장프로그램 설치.
+  // async, await의 의미 : axios가 끝날 때까지 기다렸다가 계속한다. axios가 시간이 좀 걸리기 때문.
+  getMovies = async () => {
+    const {
+      data: {
+        data : { movies }
+      }
+    }  = await axios.get("https://yts.mx/api/v2/list_movies.json") // JSON 취득
+    console.log(movies) // {data: {data : { movies }}} = movies.data.data.movies
+    this.setState({ movies, isLoading: false })
   }
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false })
-    }, 6000) // 6초 후 로딩이 완료될 것이다.
+    this.getMovies()
   }
   render() {
     const { isLoading } = this.state
