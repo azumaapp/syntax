6150c08633c9258ba93b27531ceac7cd7ce5bfdd
DESKTOP-JFUUC6H\DEV01 <ydhvocal@gmail.com>
2022-01-10 20:41:37 +0900

    js 커리큘럼 수정 (클론코딩 삭제)

--- front_css_3_bootstrap_basic1/3_mysite/assets/js/main.js
+++
@@ -1,8 +0,0 @@
-/**
- * 메뉴를 클릭했을 때 흰색으로 만듭니다.
- * @param {Number} no 메뉴번호
- */
-function activeMenu(no) {
-    let target = document.querySelector(`#menu${no}`)
-    target.classList.add("active")
-}
