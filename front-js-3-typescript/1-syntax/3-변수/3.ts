6ced7a20e24b82910940320c203b16e172584b09
azumaapp <ydhvocal@gmail.com>
2022-05-24 02:33:27 +0900

    타입스크립트 - 펜딩

--- front-js-3-typescript/1-syntax/3-변수/3.ts
+++ front-js-3-typescript/1-syntax/3-변수/3.ts
@@ -1,54 +1,24 @@
 /*
   [타입스크립트 기본 타입]
 
-  타입스크립트의 기본 타입 12가지 : Boolean, Number, String, Object, Array, Tuple, Enum, Any, Void, Null, Undefined, Never
+  타입스크립트의 기본 타입 12가지 : Any, String, Number, Boolean, Array, Tuple, Object, Enum / Void, Null, Undefined, Never
   사전준비 : node.js LTS버전, vscode 확장중 eslint, tslint, live server 설치(이후, "ctrl(cmd) + shift + p"로 live server를 검색해서 실행)
  */
 
-// 타입 표기(Type Annotation) : ":"를 이용하여 자바스크립트 코드에 타입을 정의하는 방식
-let str: string = 'hi'
-let num: number = 10
-let isLoggedIn: boolean = false
-let arr1: number[] = [1,2,3] // 배열 간단 선언
-let arr2: Array<number> = [1,2,3] // 배열 제너릭 선언
-let tuple: [string, number] = ['hi', 10]
+// 변수 타입 표기(Type Annotation) : ":"를 이용하여 자바스크립트 코드에 타입을 정의하는 방식
+let any1: any = 1 + '1' // 타입을 지정하지 않음. 기존에 자바스크립트로 구현되어 있는 웹 서비스 코드에 타입스크립트를 점진적으로 적용할 때 활용하면 좋은 타입
+let str1: string = 'hi' // 문자열
+let num1: number = 10 // 숫자
+let bool1: boolean = false // 불린
+let arr1: Array<number> = [1, 2, 3] // 숫자 배열: 제너릭 선언
+let arr2: Array<string> = ['yang', 'kang', 'hwang'] // 문자열 배열: 제너릭 선언
+let arr3: number[] = [1, 2, 3] // 숫자 배열: 제네릭 배열보다 간단한 표현식
+let arr4: string[] = ['yang', 'kang', 'hwang'] // 숫자 배열: 제네릭 배열보다 간단한 표현식
+let tuple1: [string, number] = ['hi', 10] // 튜플: 배열을 인덱스별 타입 지정하고 선언
+let obj1: object = {id: 'yang123', no: 154} // 객체를 키별 타입 지정하지 않고 선언
+let obj2: {id: string, no: number} = {id: 'yang', no: 154} // 객체를 키별 타입 지정하고 선언
+let json1: {id: string, no: number}[] = [{id: 'yang', no: 154}, {id: 'kang', no: 155}] // JSON타입
 
-arr[1].concat('!'); // Error, 'number' does not have 'concat'
-arr[5] = 'hello'; // Error, Property '5' does not exist on type '[string, number]'.
-// #Enum
-// 이넘은 C, Java와 같은 다른 언어에서 흔하게 쓰이는 타입으로 특정 값(상수)들의 집합을 의미합니다.
-
-enum Avengers { Capt, IronMan, Thor }
-let hero: Avengers = Avengers.Capt;
-// 이넘은 인덱스 번호로도 접근할 수 있습니다.
-
-enum Avengers { Capt, IronMan, Thor }
-let hero: Avengers = Avengers[0];
-// 만약 원한다면 이넘의 인덱스를 사용자 편의로 변경하여 사용할 수도 있습니다.
-
-enum Avengers { Capt = 2, IronMan, Thor }
-let hero: Avengers = Avengers[2]; // Capt
-let hero: Avengers = Avengers[4]; // Thor
-// #Any
-// 기존에 자바스크립트로 구현되어 있는 웹 서비스 코드에 타입스크립트를 점진적으로 적용할 때 활용하면 좋은 타입입니다. 단어 의미 그대로 모든 타입에 대해서 허용한다는 의미를 갖고 있습니다.
-
-let str: any = 'hi';
-let num: any = 10;
-let arr: any = ['a', 2, true];
-// #Void
-// 변수에는 undefined와 null만 할당하고, 함수에는 반환 값을 설정할 수 없는 타입입니다.
-
-let unuseful: void = undefined;
-function notuse(): void {
-  console.log('sth');
-}
-// #Never
-// 함수의 끝에 절대 도달하지 않는다는 의미를 지닌 타입입니다.
-
-// 이 함수는 절대 함수의 끝까지 실행되지 않는다는 의미
-function neverEnd(): never {
-  while (true) {
-
-  }
-}
-// ← Why TypeScript?함수 →
+// Null 타입 표기 : 휴지를 값이라 치환했을 때, null은 빈 휴지심만 있는 것. undefiend는 휴지심도 없는 것으로 생각하면 쉽다.
+let void1: void = undefined // Void : 변수에는 undefined와 null만 할당하고, 함수에는 반환 값을 설정할 수 없는 타입
+function neverEnd(): never { while (true) {} } // Never : 함수의 끝에 절대 도달하지 않는다는 의미를 지닌 타입
