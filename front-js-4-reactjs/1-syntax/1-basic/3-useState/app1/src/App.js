5d5d74e1a53a522bdcd13755039ae528ad7c6a9f
azumaapp <ydhvocal@gmail.com>
2022-05-28 18:47:06 +0900

    구조변경2

---
+++ front-js-4-reactjs/1-syntax/1-basic/3-useState/app1/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [id, setId] = useState('')
+  const [pw, setPw] = useState('')
+  const onSubmit = (event) => {
+    event.preventDefault() // form submit 고유의 동작을 멈추기 위해 일시적으로 썼음
+    console.log(id, pw)
+  }
+  
+  return (
+    <div className='App'>
+      <form onSubmit={onSubmit}>
+        <input 
+          type='text' 
+          placeholder='id를 입력해주세요'
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        /><br/>
+        <input 
+          type='text' 
+          placeholder='pw를 입력해주세요'
+          value={pw}
+          onChange={(e) => setPw(e.target.value)}
+        /><br/>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-js-4-reactjs/3-apps/2-app-react-shopping-2020/README.md
@@ -0,0 +1,4 @@
+# Shopping App
+
+React JS Fundamentals Course
+리덕스로 스테이트 관리하고, 훅스로 라이프사이클 구현해서, 함수형 컴포넌트(2019년 이후방식)로 쇼핑앱을 만들어보기
