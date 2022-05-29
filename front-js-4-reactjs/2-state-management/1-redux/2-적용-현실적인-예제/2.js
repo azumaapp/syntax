56509018a626f335bc758cbb940eaa96a61e9b66
azumaapp <ydhvocal@gmail.com>
2022-05-30 00:32:43 +0900

    redux-thunk 시작

--- front-js-4-reactjs/2-state-management/1-redux/2-적용-현실적인-예제/2.js
+++ front-js-4-reactjs/2-state-management/1-redux/2-적용-현실적인-예제/2.js
@@ -77,32 +77,32 @@ const addPost = (payload) => {
 
 
 
-// dispatch 생성 : action을 불러서 실행한다.
+// dispatch 생성 : "logIn" action을 불러서 실행한다.
 store.dispatch(logIn({
     id: 1,
     name: 'yang',
     admin: true,
 }))
 
-// store 호출 : action 일어난 후
+// store 호출 : "logIn" action 일어난 후
 console.log('2. logIn 실행 후: ', store.getState())
 
 
 
-// dispatch 생성 : action을 불러서 실행한다.
+// dispatch 생성 : "addPost" action을 불러서 실행한다.
 store.dispatch(addPost({
     userId: 1,
     id: 1,
     content: 'Hello Redux',
 }))
 
-// store 호출 : action 일어난 후
+// store 호출 : "addPost" action 일어난 후
 console.log('3. addPost 실행 후: ', store.getState())
 
 
 
-// dispatch 생성 : action을 불러서 실행한다.
+// dispatch 생성 : "logOut" action을 불러서 실행한다.
 store.dispatch(logOut())
 
-// store 호출 : action 일어난 후
+// store 호출 : "logOut" action 일어난 후
 console.log('4. logOut 실행 후: ', store.getState())
