1c489759648f51e0ce7ae906c5323d18681e17d2
azumaapp <ydhvocal@gmail.com>
2022-06-09 13:33:51 +0900

    reactjs 영화앱 문법추가

--- front-js-4-reactjs/1-syntax/1-basic/12-movie-props-reverse/app1/src/App.js
+++ front-js-4-reactjs/1-syntax/1-basic/12-movie-props-reverse/app1/src/App.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Movie from './components/Movie'
+import MovieForm from "./components/MovieForm"
 
 function App() {
-  const movies = [
+  const [movies, setMovies] = useState([
     { title: 'movie1', year: 2011 },
     { title: 'movie2', year: 2012 },
     { title: 'movie3', year: 2013 },
     { title: 'movie4', year: 2014 },
     { title: 'movie5', year: 2015 },
-  ]
+  ])
+  useEffect(() => {
+      console.log('render')
+  })
+
   const render = movies.map((movie) => {
     return (
       <Movie movie={movie} key={movie.title} />
     )
   })
+
+  const addMovie = (movie) => {    
+    setMovies([
+      ...movies,
+      movie
+    ])
+  }
+
   return (
     <div className='App'>
-      <h1>컴포넌트 + 프롭스 활용 = 무비리스트 컴포넌트화</h1>
-
+      <h1>무비 리스트</h1>
+      <MovieForm addMovie={addMovie} />
       {render}
-
     </div>
   );
 }
